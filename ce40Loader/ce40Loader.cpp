e9b91462ec4cb99d65dd0b9c09cd35dccfd30315
Michael D Peters <mpeters@embeddedarm.com>
2016-10-13 15:11:04 -0700

    Base Commit:  Project created.

--- ce40Loader/ce40Loader.cpp
+++ ce40Loader/ce40Loader.cpp
@@ -1,8 +1,18 @@
 //  ce40Loader.cpp : Defines the entry point for the console application.
 //
+// c. 2016 Technologic Systems   --- http://www.embeddedarm.com
+//  Written by Michael D. Peters
+// This program demonstrates a bit-banged approach to loading the bitmap file
+//  for the TS-4900 ICE40 FPGA.
+// This software requires a 3rd party SDK, visit www.guruce.com for the 
+//  i.MX6 TS-4900 Windows Embedded Compact 2013 SDK.
+//
+// Information on the TS-4900 is available at http://www.embeddedarm.com/products/TS-4900
 
 #include "stdafx.h"
-
+#include <windows.h>
+#include <iostream>
+#include <gpiosdk.h>  // This header is in the GuruCE SDK.
 
 int wmain(int argc, wchar_t *argv[])
 {
