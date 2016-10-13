82c9d843bb2e4af96275a26b6044a9b0f3f57f8d
Michael D Peters <mpeters@embeddedarm.com>
2016-10-13 15:03:43 -0700

    Add project files.

---
+++ ce40Loader/ce40Loader.cpp
@@ -0,0 +1,11 @@
+//  ce40Loader.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int wmain(int argc, wchar_t *argv[])
+{
+	printf("Welcome to Windows Embedded Project System \n");
+	return 0;
+}
---
+++ ce40Loader/stdafx.h
@@ -0,0 +1,27 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+
+#define _CE_ALLOW_SINGLE_THREADED_OBJECTS_IN_MTA
+#define _CE_DCOM
+
+#include "targetver.h"
+
+#include <stdio.h>
+//#include <tchar.h>
+
+
+
+// TODO: reference additional headers your program requires here
+
