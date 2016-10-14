54bba2dd1b656d0154086a6baa1df4147c6c5ef7
Michael D Peters <mpeters@embeddedarm.com>
2016-10-14 16:38:01 -0700

    Revised gpio declarations.

--- ce40Loader/ce40Loader.cpp
+++ ce40Loader/ce40Loader.cpp
@@ -30,28 +30,32 @@
 
 int wmain(int argc, wchar_t *argv[])
 {
+	int err = 0;
+
 	printf("This is the FPGA loader.  Thanks, I'll take it from here.\n");
 
 	// set up the GPIO
 	GPIO fpga_cs, mosi, miso, spi_clk, fpga_done, fpga_reset;
-	GPIO_CONFIG fpga_csConfig, mosiConfig, misoConfig, spi_clkConfig, fpga_doneConfig, fpga_resetConfig;
+	GPIO_CONFIG gpioConfig;
+	memset(&gpioConfig, 0, sizeof(gpioConfig));
+	gpioConfig.Interrupt = GPIO_INTR_NONE;
+	gpioConfig.Hysteresis = GPIO_HYSTERESIS_DISABLE;
+	gpioConfig.Function = GPIO_FUNCTION_ALT5;
+	gpioConfig.OpenDrain = GPIO_OPENDRAIN_DISABLE;
+	gpioConfig.Loopback = GPIO_LOOPBACK_DISABLE;
+	gpioConfig.Slew = GPIO_SLEW_FAST;
 
 	if (GpioInit()) {
 		// FPGA Chip Select is CSI0_DAT16 -> L4 -> gpio port 6 pin 2.
 		fpga_cs.Port = GPIO_PORT_6;
 		fpga_cs.Pin = GPIO_PIN_2;
 		fpga_cs.Value = 1;
-		fpga_csConfig.Port = fpga_cs.Port;
-		fpga_csConfig.Pin = fpga_cs.Pin;
-		fpga_csConfig.Direction = GPIO_DIR_OUT;
-		fpga_csConfig.Hysteresis = GPIO_HYSTERESIS_DISABLE;
-		fpga_csConfig.Function = GPIO_FUNCTION_ALT5;
-		fpga_csConfig.OpenDrain = GPIO_OPENDRAIN_DISABLE;
-		fpga_csConfig.Loopback = GPIO_LOOPBACK_DISABLE;
-		fpga_csConfig.Pull = GPIO_PULL_UP_100K;
-		fpga_csConfig.Slew = GPIO_SLEW_FAST;
-		fpga_csConfig.Drive = GPIO_DRIVE_34_OHM;
-		if (!GpioSetConfig(&fpga_csConfig)) {
+		gpioConfig.Port = fpga_cs.Port;
+		gpioConfig.Pin = fpga_cs.Pin;
+		gpioConfig.Direction = GPIO_DIR_OUT;
+		gpioConfig.Pull = GPIO_PULL_UP_100K;
+		gpioConfig.Drive = GPIO_DRIVE_34_OHM;
+		if (!GpioSetConfig(&gpioConfig)) {
 			// do error thing
 		};
 
@@ -59,17 +63,12 @@ int wmain(int argc, wchar_t *argv[])
 		mosi.Port = GPIO_PORT_5;
 		mosi.Pin = GPIO_PIN_27;
 		mosi.Value = 0;
-		mosiConfig.Port = mosi.Port;
-		mosiConfig.Pin = mosi.Pin;
-		mosiConfig.Direction = GPIO_DIR_OUT;
-		mosiConfig.Hysteresis = GPIO_HYSTERESIS_DISABLE;
-		mosiConfig.Function = GPIO_FUNCTION_ALT5;
-		mosiConfig.OpenDrain = GPIO_OPENDRAIN_DISABLE;
-		mosiConfig.Loopback = GPIO_LOOPBACK_DISABLE;
-		mosiConfig.Pull = GPIO_PULL_UP_100K;
-		mosiConfig.Slew = GPIO_SLEW_FAST;
-		mosiConfig.Drive = GPIO_DRIVE_34_OHM;
-		if (!GpioSetConfig(&mosiConfig)) {
+		gpioConfig.Port = mosi.Port;
+		gpioConfig.Pin = mosi.Pin;
+		gpioConfig.Direction = GPIO_DIR_OUT;
+		gpioConfig.Pull = GPIO_PULL_UP_100K;
+		gpioConfig.Drive = GPIO_DRIVE_34_OHM;
+		if (!GpioSetConfig(&gpioConfig)) {
 			// do error thing
 		}
 
@@ -77,15 +76,12 @@ int wmain(int argc, wchar_t *argv[])
 		miso.Port = GPIO_PORT_5;
 		miso.Pin = GPIO_PIN_28;
 		miso.Value = 0;  // it's an input.
-		misoConfig.Direction = GPIO_DIR_IN;
-		misoConfig.Hysteresis = GPIO_HYSTERESIS_DISABLE;
-		misoConfig.Function = GPIO_FUNCTION_ALT5;
-		misoConfig.OpenDrain = GPIO_OPENDRAIN_ENABLE; // I don't know if this is correct.
-		misoConfig.Loopback = GPIO_LOOPBACK_DISABLE;
-		misoConfig.Pull = GPIO_PULL_INVALID; // I don't think we want to put voltage on this pin.
-		misoConfig.Slew = GPIO_SLEW_FAST;
-		misoConfig.Drive = GPIO_DRIVE_DISABLED;  // Don't drive this pin.
-		if (!GpioSetConfig(&misoConfig)) {
+		gpioConfig.Port = miso.Port;
+		gpioConfig.Pin = miso.Pin;
+		gpioConfig.Direction = GPIO_DIR_IN;
+		gpioConfig.Pull = GPIO_PULL_INVALID; // I don't think we want to put voltage on this pin.
+		gpioConfig.Drive = GPIO_DRIVE_DISABLED;  // Don't drive this pin.
+		if (!GpioSetConfig(&gpioConfig)) {
 			// do error thing
 		}
 
@@ -93,15 +89,12 @@ int wmain(int argc, wchar_t *argv[])
 		spi_clk.Port = GPIO_PORT_5;
 		spi_clk.Pin = GPIO_PIN_26;
 		spi_clk.Value = 1;
-		spi_clkConfig.Direction = GPIO_DIR_OUT;
-		spi_clkConfig.Hysteresis = GPIO_HYSTERESIS_DISABLE;
-		spi_clkConfig.Function = GPIO_FUNCTION_ALT5; // api says this is ignored for setup of gpio.
-		spi_clkConfig.OpenDrain = GPIO_OPENDRAIN_DISABLE;
-		spi_clkConfig.Loopback = GPIO_LOOPBACK_DISABLE;
-		spi_clkConfig.Pull = GPIO_PULL_UP_100K;  // Weak pull up?  Maybe need stronger.  CLK should idle high.
-		spi_clkConfig.Slew = GPIO_SLEW_FAST;
-		spi_clkConfig.Drive = GPIO_DRIVE_34_OHM;
-		if (!GpioSetConfig(&spi_clkConfig)) {
+		gpioConfig.Port = spi_clk.Port;
+		gpioConfig.Pin = spi_clk.Pin;
+		gpioConfig.Direction = GPIO_DIR_OUT;
+		gpioConfig.Pull = GPIO_PULL_UP_100K;  // Weak pull up?  Maybe need stronger.  CLK should idle high.
+		gpioConfig.Drive = GPIO_DRIVE_34_OHM;
+		if (!GpioSetConfig(&gpioConfig)) {
 			// do error thing
 		}
 
@@ -109,15 +102,12 @@ int wmain(int argc, wchar_t *argv[])
 		fpga_done.Port = GPIO_PORT_5;
 		fpga_done.Pin = GPIO_PIN_20;
 		fpga_done.Value = 0;
-		fpga_doneConfig.Direction = GPIO_DIR_IN;
-		fpga_doneConfig.Hysteresis = GPIO_HYSTERESIS_DISABLE;
-		fpga_doneConfig.Function = GPIO_FUNCTION_ALT5;
-		fpga_doneConfig.OpenDrain = GPIO_OPENDRAIN_DISABLE; 
-		fpga_doneConfig.Loopback = GPIO_LOOPBACK_DISABLE;
-		fpga_doneConfig.Pull = GPIO_PULL_UP_100K; // weak pull.  I want the fpga to drive this low.
-		fpga_doneConfig.Slew = GPIO_SLEW_FAST;
-		fpga_doneConfig.Drive = GPIO_DRIVE_DISABLED;  // this is an input, don't drive it.
-		if (!GpioSetConfig(&fpga_doneConfig)) {
+		gpioConfig.Port = fpga_done.Port;
+		gpioConfig.Pin = fpga_done.Pin;
+		gpioConfig.Direction = GPIO_DIR_IN;
+		gpioConfig.Pull = GPIO_PULL_UP_100K; // weak pull.  I want the fpga to drive this low.
+		gpioConfig.Drive = GPIO_DRIVE_DISABLED;  // this is an input, don't drive it.
+		if (!GpioSetConfig(&gpioConfig)) {
 			// do error thing
 		}
 
@@ -125,18 +115,33 @@ int wmain(int argc, wchar_t *argv[])
 		fpga_reset.Port = GPIO_PORT_5;
 		fpga_reset.Pin = GPIO_PIN_21;
 		fpga_reset.Value = 1; 
-		fpga_resetConfig.Direction = GPIO_DIR_OUT;
-		fpga_resetConfig.Hysteresis = GPIO_HYSTERESIS_DISABLE;
-		fpga_resetConfig.Function = GPIO_FUNCTION_ALT5;
-		fpga_resetConfig.OpenDrain = GPIO_OPENDRAIN_DISABLE;
-		fpga_resetConfig.Loopback = GPIO_LOOPBACK_DISABLE;
-		fpga_resetConfig.Pull = GPIO_PULL_UP_47K;
-		fpga_resetConfig.Slew = GPIO_SLEW_FAST;
-		fpga_resetConfig.Drive = GPIO_DRIVE_34_OHM;
-		if (!GpioSetConfig(&fpga_resetConfig)) {
+		gpioConfig.Port = fpga_reset.Port;
+		gpioConfig.Pin = fpga_reset.Pin;
+		gpioConfig.Direction = GPIO_DIR_OUT;
+		gpioConfig.Pull = GPIO_PULL_UP_47K;
+		gpioConfig.Drive = GPIO_DRIVE_34_OHM;
+		if (!GpioSetConfig(&gpioConfig)) {
 			// do error thing
 		}
 	}
+
+#define TEST
+#ifdef TEST
+	err = GetLastError();
+	// Check gpio pin functions.
+	if (!GpioWritePin(&fpga_reset))
+		std::cout << "Couldn't write fpga reset." << std::endl;
+	if (!GpioReadPin(&fpga_done) && (err = GetLastError()))
+		std::cout << "Couldn't read FPGA done, error code " << err << "." << std::endl;
+	if (!GpioReadPin(&miso) && (err = GetLastError()))
+		std::cout << "Couldn't read miso, error code " << err << "." << std::endl;
+	if (!GpioWritePin(&mosi))
+		std::cout << "Couldn't set mosi." << std::endl;
+	if (!GpioWritePin(&fpga_cs))
+		std::cout << "Couldn't set fpga chip select." << std::endl;
+	if (!GpioWritePin(&spi_clk))
+		std::cout << "Couldn't tick spi clock." << std::endl;
+#endif
 
 	// load the file
 
